b10cdaded8d59692a0a49abab42f7bfc09979620
Uladzimir86 <77360741+Uladzimir86@users.noreply.github.com>
2024-11-12 23:58:16 +0300

    solve formatTime

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -235,8 +235,9 @@ function endsWith(str, substr) {
  *   formatTime(0, 45) => "00:45"
  *   formatTime(0, 0) => "00:00"
  */
-function formatTime(/* minutes, seconds */) {
-  throw new Error('Not implemented');
+function formatTime(minutes, seconds) {
+  const addZero = (num) => String(num).padStart(2, 0);
+  return `${addZero(minutes)}:${addZero(seconds)}`;
 }
 
 /**
